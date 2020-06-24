6727dea18768f9d6eaa31db0be354d8476bf875f
chinazman <chinazman@qq.com>
2020-06-25 07:36:34 +0800

    change dir

--- v2ray/initdocker.sh
+++ v2ray/initdocker.sh
@@ -5,5 +5,5 @@ rm -f containerd.io-1.2.6-3.3.el7.x86_64.rpm
 wget -qO- https://get.docker.com/ | sh
 systemctl start docker
 mkdir /root/v2ray
-wget -O /root/v2ray https://raw.githubusercontent.com/niubude/xtools/master/v2ray/config.json
+wget -O /root/v2ray/config.json https://raw.githubusercontent.com/niubude/xtools/master/v2ray/config.json
 docker run --name=v2ray -p 8888:8888 -v /root/v2ray:/etc/v2ray -d v2ray/official

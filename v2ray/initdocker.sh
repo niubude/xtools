fa820391a0b61838147b11c819207a4257bbf8bc
chinazman <chinazman@qq.com>
2020-06-25 07:03:08 +0800

    v2ray

---
+++ v2ray/initdocker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+wget https://download.docker.com/linux/centos/7/x86_64/edge/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+yum -y install containerd.io-1.2.6-3.3.el7.x86_64.rpm
+rm -f containerd.io-1.2.6-3.3.el7.x86_64.rpm
+wget -qO- https://get.docker.com/ | sh
+systemctl start docker
+mkdir /root/v2ray
+wget -O /root/v2ray https://raw.githubusercontent.com/niubude/xtools/master/v2ray/config.json
+docker run --name=v2ray -p 8888:8888 -v /root/v2ray:/etc/v2ray -d v2ray/official
